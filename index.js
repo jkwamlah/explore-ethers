5fc5ceb22370c5cf76fb43862a66bcbd0e9ea158
jkwamlah <jkwamlah@outlook.com>
2023-08-23 15:28:39 +0000

    refactored index.js for wallet with no address

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import {ethers} from 'ethers'
-import {PRIVATE_KEY_0, PRIVATE_KEY_1, PRIVATE_KEY_2, PRIVATE_KEY_3, PRIVATE_KEY_4, PRIVATE_KEY_5, PRIVATE_KEY_6, PRIVATE_KEY_7, wallet8, wallet9, ganacheProvider
+import {
+    PRIVATE_KEY_0,
+    PRIVATE_KEY_1,
+    PRIVATE_KEY_2,
+    PRIVATE_KEY_3,
+    PRIVATE_KEY_4,
+    PRIVATE_KEY_5,
+    PRIVATE_KEY_6,
+    PRIVATE_KEY_7,
+    wallet8,
+    wallet9,
+    ganacheProvider
 } from './config.js'
 
 const {utils, providers, Wallet} = ethers
@@ -19,58 +30,38 @@ const wallet7 = new Wallet(PRIVATE_KEY_7, provider);
     console.log("balance wallet2: ", utils.formatEther(await wallet2.getBalance()))
     console.log("balance wallet3: ", utils.formatEther(await wallet3.getBalance()))
     console.log("balance wallet4: ", utils.formatEther(await wallet4.getBalance()))
-    console.log("balance wallet5: ", utils.formatEther(await wallet5.getBalance()))
-    console.log("balance wallet6: ", utils.formatEther(await wallet6.getBalance()))
-    console.log("balance wallet7: ", utils.formatEther(await wallet7.getBalance()))
 
     // Transfer funds from wallet1 to other wallets
-    await payroll(1, wallet1, [
-        wallet2.address,
-        wallet3.address,
-        wallet4.address,
-        wallet5.address,
-        wallet6.address,
-    ]);
-
+    await payroll(1.0, wallet1, [wallet2.address, wallet3.address, wallet4.address, wallet5.address, wallet6.address]);
     // Transfer funds from wallet2 to other wallets
-    await payroll(2, wallet2, [
-        wallet0.address,
-        wallet4.address,
-        wallet5.address,
-        wallet6.address,
-        wallet7.address,
-    ]);
+    await payroll(2.0, wallet2, [wallet0.address, wallet4.address, wallet5.address, wallet6.address, wallet7.address]);
 
     console.log("balance after wallet0: ", utils.formatEther(await wallet0.getBalance()))
     console.log("balance after wallet1: ", utils.formatEther(await wallet1.getBalance()))
     console.log("balance after wallet2: ", utils.formatEther(await wallet2.getBalance()))
     console.log("balance after wallet3: ", utils.formatEther(await wallet3.getBalance()))
     console.log("balance after wallet4: ", utils.formatEther(await wallet4.getBalance()))
-    console.log("balance after wallet5: ", utils.formatEther(await wallet5.getBalance()))
-    console.log("balance after wallet6: ", utils.formatEther(await wallet6.getBalance()))
-    console.log("balance after wallet7: ", utils.formatEther(await wallet7.getBalance()))
 
     const wallet1Addresses = await findRecipientAddresses(wallet1.address)
-    console.log("addresses for wallet 1: ", wallet1Addresses)
+    const emptyWalletAddresses = "No data found"
+    console.log("addresses for wallet 1: ", wallet1Addresses.length ? wallet1Addresses : emptyWalletAddresses)
 
     const wallet2Addresses = await findRecipientAddresses(wallet2.address)
-    console.log("addresses for wallet 2: ", wallet2Addresses)
+    console.log("addresses for wallet 2: ", wallet2Addresses.length ? wallet2Addresses : emptyWalletAddresses)
+
+    const wallet3Addresses = await findRecipientAddresses(wallet3.address)
+    console.log("addresses for wallet 3: ", wallet3Addresses.length ? wallet3Addresses : emptyWalletAddresses)
 })();
-
-
 
 async function payroll(amount, sender, employees) {
     const GAS = 50 // in WEI
-    if(amount <= 0 || employees.length==0) return
+    if (amount <= 0 || employees.length == 0) return
     const senderBalance = await sender.getBalance()
     const amountInWei = utils.parseUnits(amount.toString(), 18)
 
-    if( senderBalance >= ((employees.length * amountInWei) + GAS)) {
-        for(let i=0; i<employees.length; i++) {
-            await sender.sendTransaction({
-                value: amountInWei,
-                to: employees[i],
-            })
+    if (senderBalance >= ((employees.length * amountInWei) + GAS)) {
+        for (let i = 0; i < employees.length; i++) {
+            await sender.sendTransaction({value: amountInWei, to: employees[i]})
         }
     } else {
         console.log('it didnt work...')
@@ -83,7 +74,6 @@ async function findRecipientAddresses(address) {
 
     for (let blockNumber = 0; blockNumber <= latestBlockNumber; blockNumber++) {
         const block = await provider.getBlockWithTransactions(blockNumber);
-
         const filteredTransactionAddress = block.transactions.filter(
             transaction => transaction.from === address
         );
